f28c7f7166b5349724146170402dceddc3b6d4fb
Шиманов Дмитрий Анатольевич <d.a.shimanov@gmail.com>
2018-03-02 14:28:46 +0700

    Добавил класс модели Movie

---
+++ MovieDb/MovieDbLibrary/Model/Movie.cs
@@ -0,0 +1,30 @@
+﻿namespace MovieDbLibrary.Model
+{
+
+    public class Root
+    {
+        public int Page { get; set; }
+        public int TotalResults { get; set; }
+        public int TotalPages { get; set; }
+        public Movie[] MovieResults { get; set; }
+    }
+
+    public class Movie
+    {
+        public int VoteCount { get; set; }
+        public int Id { get; set; }
+        public bool Video { get; set; }
+        public float VoteAverage { get; set; }
+        public string Title { get; set; }
+        public float Popularity { get; set; }
+        public string PosterPath { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string OriginalTitle { get; set; }
+        public int[] GenreIds { get; set; }
+        public string BackdropPath { get; set; }
+        public bool Adult { get; set; }
+        public string Overview { get; set; }
+        public string ReleaseDate { get; set; }
+    }
+
+}
